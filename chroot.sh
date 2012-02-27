cf06d33a798a9c3f96205653d73a457ebe1db0a9
Sebastian Simon <sebastian.simon@online.de>
2012-02-27 16:11:03 +0100

    added license-information

--- chroot.sh
+++ chroot.sh
@@ -4,11 +4,16 @@
 # danach werden /dev, /proc und /sys gemountet und chroot
 # ausgeführt.
 
+
 # mounten der Laufwerke
 echo "Laufwerke einhängen..."
-mount -O noatime,discard UUID="b5632168-fd83-423e-92ea-e36b59055ea4" /mnt/gentoo &&      # Das root-Lfw (/dev/sdb4)
-mount -O noatime UUID="d541458a-70b4-4a94-b3d8-c48a3119d01d" /mnt/gentoo/usr/portage &&  # Der portage-tree (dev/vg_182tib/portage_tree)
-mount -o noatime,discard UUID="01f75b97-df45-4ff5-be55-6bc5753132ca" /mnt/gentoo/boot && # Das /boot-Lfw (/dev/sdb2)
+
+# Das root-Lfw (/dev/sdb4)
+mount -o noatime,discard UUID="b5632168-fd83-423e-92ea-e36b59055ea4" /mnt/gentoo &&
+# Der portage-tree (dev/vg_182tib/portage_tree)
+mount -o noatime UUID="d541458a-70b4-4a94-b3d8-c48a3119d01d" /mnt/gentoo/usr/portage &&
+# Das /boot-Lfw (/dev/sdb2)
+mount -o noatime,discard UUID="01f75b97-df45-4ff5-be55-6bc5753132ca" /mnt/gentoo/boot &&
 echo "...Fertig"
 
 # mounten der Spezialordner
@@ -21,16 +26,16 @@ echo "...Fertig"
 # chroot ausführen (bevorzugt mit ZSH, sonst mit BASH)
 echo "chroot ausführen..."
 if [ -e /mnt/gentoo/bin/zsh ]
-	then
-		echo "ZSH gefunden und wird verwendet..."
-		chroot /mnt/gentoo /bin/bash &&
-	else
-		echo "BASH wird verwendet..."
-		chroot /mnt/gentoo /bin/bash &&
+    then
+        echo "ZSH gefunden und wird verwendet..."
+        chroot /mnt/gentoo /bin/bash &&
+    else
+        echo "BASH wird verwendet..."
+        chroot /mnt/gentoo /bin/bash &&
 fi
 echo "...Fertig"
 
-# unmounten der Laufwerke (Umgekehrte Reihenfolge beachten!)
+# unmounten der Laufwerke (Reihenfolge beachten)
 echo "Spezialordner und Laufwerke aushängen..."
 umount -l /mnt/gentoo/sys &&
 umount -l /mnt/gentoo/proc &&
@@ -39,5 +44,3 @@ umount -l /mnt/gentoo/usr/portage &&
 umount -l /mnt/gentoo/boot &&
 umount -l /mnt/gentoo &&
 echo "...Fertig"
-echo ""
-echo "UND RAUS"
